58e2f5fdb83ca963f09d396f80fe4fd38a37f365
Hendrik Gani <hendrik.gani@gmail.com>
2018-04-01 13:35:32 +1000

    add test code

--- Tipper/mobile/src/main/java/io/tipper/tipper/TipperHome.java
+++ Tipper/mobile/src/main/java/io/tipper/tipper/TipperHome.java
@@ -7,6 +7,14 @@ import android.widget.Button;
 
 import com.gani.lib.logging.GLog;
 
+import org.web3j.protocol.Web3j;
+import org.web3j.protocol.Web3jFactory;
+import org.web3j.protocol.core.methods.response.Web3ClientVersion;
+import org.web3j.protocol.http.HttpService;
+
+import java.io.IOException;
+import java.util.concurrent.ExecutionException;
+
 import io.tipper.tipper.app.database.MyDbValue;
 
 public class TipperHome extends AppCompatActivity {
@@ -35,5 +43,33 @@ public class TipperHome extends AppCompatActivity {
                 startActivity(sendActivity.intent());
             }
         });
+
+//        Web3j web3 = Web3j.build(new HttpService());  // defaults to http://localhost:8545/
+//        Web3ClientVersion web3ClientVersion = web3.web3ClientVersion().send();
+//        String clientVersion = web3ClientVersion.getWeb3ClientVersion();
+
+//        Web3j web3 = Web3j.build(new HttpService());  // defaults to http://localhost:8545/
+//        Web3ClientVersion web3ClientVersion = web3.web3ClientVersion().sendAsync().get();
+//        String clientVersion = web3ClientVersion.getWeb3ClientVersion();
+
+
+        Web3j web3 = Web3jFactory.build(new HttpService("https://rinkeby.infura.io/tQmR2iidoG7pjW1hCcCf"));  // defaults to http://localhost:8545/
+        try {
+            Web3ClientVersion web3ClientVersion = web3.web3ClientVersion().sendAsync().get();
+
+//            Web3ClientVersion web3ClientVersion = web3.web3ClientVersion().send();
+            String clientVersion = web3ClientVersion.getWeb3ClientVersion();
+            GLog.t(getClass(), "CLIENT: " + clientVersion);
+
+        }
+//        catch (IOException e) {
+//            GLog.e(getClass(), "Failed", e);
+//        }
+        catch (InterruptedException e) {
+            GLog.e(getClass(), "Failed", e);
+        }
+        catch (ExecutionException e) {
+            GLog.e(getClass(), "Failed", e);
+        }
     }
 }
